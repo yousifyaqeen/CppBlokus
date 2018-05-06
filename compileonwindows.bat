7d8e7a8a56af039c98dfb8f6e7f202c411447ecf
ibnbattuta <yoyox98@gmail.com>
2018-05-06 17:52:08 +0200

    smaller compile size

--- compileonwindows.bat
+++ compileonwindows.bat
@@ -1,10 +1,10 @@
 rm -r build
 mkdir build
-g++ -Wall -g  -I%cd%\include -c "%cd%\blokus.cc" -o build\blokus.o 2> compiling_error.txt
-findstr "^" "compiling_error.txt" || del "compiling_error.txt"
-g++ -L%cd%\include\lib -o "build\blokus.exe" build\blokus.o   -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
+g++ -Wall -g -Wno-deprecated-declarations -I%cd%\include -c "%cd%\blokus.cc" -o build\blokus.o 
+g++ -L%cd%\include\lib -o "build\blokus.exe" build\blokus.o   -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio 
 copy  %cd%\include\bin\*.dll %cd%\build
 mkdir %cd%\build\res
 xcopy /E %cd%\res %cd%\build\res
 cd %cd%\build
+rm blokus.o
 blokus.exe
